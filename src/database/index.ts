ed40d3aaab734ae433e6b3a2392f194250b45734
Natanael Silva <natanaelsilva.progjs@gmail.com>
2021-07-21 14:13:52 -0300

    :heavy_plus_sign: Chore: Configuration setup of dependency

---
+++ src/database/index.ts
@@ -0,0 +1,4 @@
+/* eslint-disable no-console */
+import { createConnection } from 'typeorm';
+
+createConnection().then(() => console.log('✅ Successfully connected with database'));
---
+++ src/database/migrations/1626885735965-CreateUser.ts
@@ -0,0 +1,55 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateUser1626885735965 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+          },
+          {
+            name: 'active',
+            type: 'boolean',
+            default: true,
+          },
+          {
+            name: 'created_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+    await queryRunner.query('DROP EXTENSION "uuid-ossp"');
+  }
+}
