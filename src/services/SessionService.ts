ae20cf04104c6d8758f80e2adc8bab63066d6686
Natanael Silva <natanaelsilva.progjs@gmail.com>
2021-07-22 23:52:30 -0300

    :sparkles: Feat: Authentication of JWT

--- src/services/SessionService.ts
+++ src/services/SessionService.ts
@@ -35,6 +35,10 @@ class SessionService {
       throw new AppError('Credencias inválidas', 401);
     }
 
+    if (!user.active) {
+      throw new AppError('Usuário inativo', 401);
+    }
+
     const token = sign({ id: user.id }, process.env.APP_SECRET as string, {
       expiresIn: '1d',
     });
