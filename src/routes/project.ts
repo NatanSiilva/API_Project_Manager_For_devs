733033097a6cb8abde26fc32b93f8667ec2c338a
Natanael Silva <natanaelsilva.progjs@gmail.com>
2021-07-24 21:07:23 -0300

    :sparkles: Feat: Upload image with Multer

--- src/routes/project.ts
+++ src/routes/project.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from '../config/multer';
 import ProjectController from '../controllers/ProjectController';
 import authentication from '../middleware/auth';
 
@@ -9,7 +11,16 @@ projectRoutes.use(authentication);
 
 projectRoutes.get('/', projectController.index);
 projectRoutes.get('/:id', projectController.show);
-projectRoutes.post('/', projectController.create);
+projectRoutes.post(
+  '/',
+  multer(multerConfig).single('logo'),
+  projectController.create,
+);
+projectRoutes.put(
+  '/:id/upload',
+  multer(multerConfig).single('logo'),
+  projectController.UploadLogo,
+);
 projectRoutes.put('/:id', projectController.update);
 projectRoutes.patch('/:id', projectController.changeStatus);
 
